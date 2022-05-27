11b2bdcfcdcfb4f54b89a2e64a4c1208c18acc0d
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-27 14:15:00 -0300

    Ajustando o caminho da requisição

--- javascript/filmes__controller.js
+++ javascript/filmes__controller.js
@@ -1,10 +1,15 @@
-const listaFilme = () =>{
-    return fetch ('https://api.themoviedb.org/3/movie/550?api_key=6c103da2d312053157cc9821206d6474') .then (resposta=>{
-        if (resposta.ok){
-            return resposta.json();
-            
-        }
-        throw new Error ("Não foi possível listar os produtos")
+const listaFilme = async () =>{
+    const resposta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR&page=1')
+    if (resposta.ok) {
+        return resposta.json()
+
+
+    }
+    throw new Error("Não foi possível listar os produtos")
+    
+    
+    .then(data => {
+        console.log(data.results)
     })
 
     
@@ -14,4 +19,3 @@ const listaFilme = () =>{
         listaFilme
     }
 
-    
