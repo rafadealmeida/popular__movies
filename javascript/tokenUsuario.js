80e3824298d308e11b8c762bc776ac93ba89ac03
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-27 18:53:13 -0300

    Gerar uma id de sessão para o usuário

--- javascript/tokenUsuario.js
+++ javascript/tokenUsuario.js
@@ -1,9 +1,27 @@
-const listaFilme = async () =>{
+const loginSessaoConvidado = async () =>{
     const resposta = await fetch('https://api.themoviedb.org/3/authentication/guest_session/new?api_key=6c103da2d312053157cc9821206d6474')
-    if (resposta.success) {
+    if (resposta.ok) {
         return resposta.json()
     }
     throw new Error("Não foi possível iniciar a Sessão")
     
 }
 
+const botao = document.querySelector(".sessao__botao")
+
+
+botao.addEventListener('click', function () {
+    sessaoConvidado()
+})
+
+const sessaoConvidado = async () =>{
+    try{
+        const token = await loginSessaoConvidado()
+        
+        const tokenID = token.guest_session_id
+        console.log(tokenID)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
