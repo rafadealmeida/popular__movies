81c292bf3afa2a00ea2b1afc4223d235cd1e58f1
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-27 18:53:49 -0300

    Usar método post para favoritar o filme

--- javascript/filmes__controller.js
+++ javascript/filmes__controller.js
@@ -15,6 +15,26 @@ const listaFilme = async () =>{
     
 }
 
+const favoritarFilme = async (tokenID) => {
+    const results = await fetch(`https://api.themoviedb.org/3/account/{account_id}/favorite?api_key=6c103da2d312053157cc9821206d6474&session_id=${tokenID}`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+                "media_type": "movie",
+                "media_id": 550,
+                "favorite": true
+              
+        })
+    })
+    
+    if (results.ok){
+        return results.json()
+    }
+    throw new Error("Não foi possível favoritar o filme")
+}
+
     export const filmesServices = {
         listaFilme
     }
