37e4a8fa5b2a35425feced299e8f6a0ca459717a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-05-28 10:03:43 -0300

    Requirindo a id da sessão via API

--- javascript/tokenUsuario.js
+++ javascript/tokenUsuario.js
@@ -7,11 +7,21 @@ const loginSessaoConvidado = async () =>{
     
 }
 
+const iniciarSessao = async () =>{
+    const resposta = await fetch('https://api.themoviedb.org/3/authentication/token/new?api_key=6c103da2d312053157cc9821206d6474')
+    if (resposta.ok) {
+        return resposta.json()
+    }
+    throw new Error("Não foi possível iniciar a Sessão")
+     
+}
+
 const botao = document.querySelector(".sessao__botao")
 
 
 botao.addEventListener('click', function () {
-    sessaoConvidado()
+    // sessaoConvidado()
+    sessaoId()
 })
 
 const sessaoConvidado = async () =>{
@@ -20,6 +30,19 @@ const sessaoConvidado = async () =>{
         
         const tokenID = token.guest_session_id
         console.log(tokenID)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+const sessaoId = async () =>{
+    try {
+        const id = await iniciarSessao()
+
+        const sessionID = id.request_token
+
+        console.log(sessionID)
     }
     catch(err){
         console.log(err)
