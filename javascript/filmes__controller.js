ea31c3af4484d18d6490f766f5f1d67f2c766a55
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-05 11:41:31 -0300

    Requisição para buscar filme por id

--- javascript/filmes__controller.js
+++ javascript/filmes__controller.js
@@ -47,10 +47,23 @@ const pesquisaFilme = async (filme) =>{
     
 }
 
+const pesquisaFilmeID = async (id) =>{
+    const resposta = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR`)
+    if (resposta.ok) {
+        return resposta.json()
+
+
+    }
+    throw new Error("Não foi possível pesquisar o filme")
+
+    
+}
+
 
     export const filmesServices = {
         listaFilme,
         favoritarFilme,
-        pesquisaFilme
+        pesquisaFilme,
+        pesquisaFilmeID
     }
 
