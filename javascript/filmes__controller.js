c10e167420463c32e4d5cd73cb16c3b197143a81
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-06 10:59:10 -0300

    limpando codigo sujo

--- javascript/filmes__controller.js
+++ javascript/filmes__controller.js
@@ -2,17 +2,9 @@ const listaFilme = async () =>{
     const resposta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR&page=1')
     if (resposta.ok) {
         return resposta.json()
-
-
     }
-    throw new Error("Não foi possível listar os filmes")
-    
-    
-    // .then(data => {
-    //     console.log(data.results)
-    // })
-
-    
+    throw new Error("Não foi possível listar os filmes")    
+  
 }
 
 const favoritarFilme = async (tokenID) => {
