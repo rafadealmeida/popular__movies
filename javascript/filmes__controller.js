1c468d689de3ba958b65245d791aa7ddd0c16650
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-09 10:23:31 -0300

    Usando a APIkey, de outro arquivo JS

--- javascript/filmes__controller.js
+++ javascript/filmes__controller.js
@@ -1,5 +1,6 @@
+import {apiKey} from './apiKey.js'
 const listaFilme = async () =>{
-    const resposta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR&page=1')
+    const resposta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`)
     if (resposta.ok) {
         return resposta.json()
     }
@@ -8,7 +9,7 @@ const listaFilme = async () =>{
 }
 
 const favoritarFilme = async (tokenID) => {
-    const results = await fetch(`https://api.themoviedb.org/3/account/{account_id}/favorite?api_key=6c103da2d312053157cc9821206d6474&session_id=${tokenID}`,{
+    const results = await fetch(`https://api.themoviedb.org/3/account/{account_id}/favorite?api_key=${apiKey}&session_id=${tokenID}`,{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -28,7 +29,7 @@ const favoritarFilme = async (tokenID) => {
 }
 
 const pesquisaFilme = async (filme) =>{
-    const resposta = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR&query=${filme}&page=1&include_adult=false`)
+    const resposta = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&query=${filme}&page=1&include_adult=false`)
     if (resposta.ok) {
         return resposta.json()
 
@@ -40,7 +41,7 @@ const pesquisaFilme = async (filme) =>{
 }
 
 const pesquisaFilmeID = async (id) =>{
-    const resposta = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6c103da2d312053157cc9821206d6474&language=pt-BR`)
+    const resposta = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}4&language=pt-BR`)
     if (resposta.ok) {
         return resposta.json()
 
