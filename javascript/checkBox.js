9093cc3619456057ee4e6a82b6ddf8a78f6c689d
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-08 21:38:41 -0300

    Criando a lógica do checkbox
    1-Escutador no checkbox, caso esteja marcado , buscara os filmes no, localStogare, caso contrário, a pagina inicial será exibida normalmente

---
+++ javascript/checkBox.js
@@ -0,0 +1,58 @@
+import { render } from "./criaCard.js"
+import { getFavoritaFilme } from "./criaCard.js"
+import{ criaCard } from './criaCard.js'
+import {favService} from "./favoritarFilme.js"
+
+const checkbox = document.querySelector(".cabecalho__checkbox--input")
+const sessaoFilmes = document.querySelector(".cards__filmes--todos")
+
+checkbox.addEventListener("change",(event) => {
+    event.currentTarget.checked  ? mostrarFavoritos() : voltarFilme()
+})
+
+const mostrarFavoritos = () =>{
+    const filmesFavoritos = getFavoritaFilme() || []
+    sessaoFilmes.innerHTML = ""
+    filmesFavoritos.forEach(filmeFav=>{
+
+        sessaoFilmes.appendChild(criaCard(filmeFav.poster_path,filmeFav.title,filmeFav.vote_average,filmeFav.overview,filmeFav.id))
+        const fav = document.querySelectorAll('.card__filme--favorito')
+    
+   
+
+        fav.forEach(favorito => {
+    
+            favorito.style.backgroundImage = favoritoEstado.favoritado
+            favorito.addEventListener('click', (event) =>{
+                let coracaoFav = event.target.style.backgroundImage
+                let id = event.target.id
+                console.log("clicou")
+                                             
+                if(coracaoFav == ""){
+                    favorito.style.backgroundImage = favoritoEstado.favoritado
+                        
+                    favService.procurarFilme(id).then(data=>{
+                        filme = data
+                        favService.saveToLocalStorage(filme)
+                    })      
+                    
+                }else{
+                    favorito.style.backgroundImage = favoritoEstado.naoFavoritado
+                    coracaoFav=""
+                    
+                    favService.removeFromLocalStorage(id)   
+                }
+            })
+        })
+    })
+}
+
+const voltarFilme = ()=>{
+    sessaoFilmes.innerHTML=""
+    render()
+
+}
+const favoritoEstado = {
+    favoritado :  "url('../src/vetores/HeartFav.svg')",
+    naoFavoritado :  "url('../src/vetores/Heart.svg')"
+}
